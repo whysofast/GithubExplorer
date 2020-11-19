fadb8460ba9d196e3c96f1c8d2d35f6ed5f6e0a9
Matheus Carvalho <matheusvca@gmail.com>
2020-11-19 01:19:29 -0300

    Start project with routes, pages and styled components

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore Github Repositories</Title>;
+};
+
+export default Dashboard;
